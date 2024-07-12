1775443360f0a2f6aba56cce1b662d206f6a89cf
encador <encador@proton.me>
2024-07-12 12:23:16 -0400

    add basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 
+import Board from "@/components/messages/Board.vue";
 </script>
 
 <template>
   <div id="application">
-
+    <Board/>
   </div>
 </template>
 
 <style scoped>
+#application{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
 </style>
--- src/components/messages/Board.vue
+++ src/components/messages/Board.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-
+  <ul id="messages">
+    <li>Message 1</li>
+    <li>Message 2</li>
+    <li>Message 3</li>
+  </ul>
 </template>
 
 <style scoped>
+ul{
+  list-style-type: none;
+  border: black solid 3px;
+  padding: 0;
+  margin: 0;
+  width: 25%;
+  height: 45%;
 
+}
 </style>
